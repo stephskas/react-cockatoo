0c044779dd75a14e31fad9135476d9d45baa02b9
stephskas <steph.sakai@gmail.com>
2023-01-15 06:32:13 -0800

    working with asynchronous data, conditional rendering, advanced state and impossible state

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import React, { useState, useEffect } from 'react';
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 
-const useSemiPersistentState = () => {
-  const [todoList, setTodoList] = useState(JSON.parse(localStorage.getItem("savedTodoList")) || []);
 
-  useEffect(() => {
-    localStorage.setItem("savedTodoList", JSON.stringify(todoList))
-  }, [todoList] )
-
-  return [todoList, setTodoList]
-}
 
 function App() {
-  const [todoList, setTodoList] = useSemiPersistentState()
+  const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+      new Promise((resolve, reject) =>
+        setTimeout(() => {
+          resolve({ data: { todoList: JSON.parse(localStorage.getItem('savedTodoList')) || [] }}
+        )}, 2000))
+      .then((result) => {
+        setTodoList(result.data.todoList)
+        setIsLoading(false)
+      })
+  }, [])
+  
   const addTodo = (newTodo) => {
-    setTodoList([...todoList, newTodo]);
+      setTodoList([...todoList, newTodo]);
   }
 
   const removeTodo = (id) => {
@@ -24,11 +28,20 @@ function App() {
     setTodoList(updatedTodos);
   }
 
+  useEffect(() => {
+    if(!isLoading) {
+      localStorage.setItem('savedTodoList', JSON.stringify(todoList))
+    }},[todoList])
+
   return (
    <> 
      <h1>Todo List</h1>
      <AddTodoForm onAddTodo={addTodo} />
-     <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>
+     {isLoading ? (
+        <p>Loading...</p> 
+        ):(
+        <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>
+      )}
    </>
   );
 }
