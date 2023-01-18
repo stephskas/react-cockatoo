16eb34bf9b68899edf4a70ca403704fccf65aab1
stephskas <steph.sakai@gmail.com>
2023-01-18 02:16:04 -0800

    Cleaned up code: Added paren in TodoList component for implicit return of TodoListItem Removed empty array from App Component useEffect which gets data from local storage when component is registered, and added spread operator to setTodoList argument to pull JSON data to into new array to prevent error in case where savedTodoList is empty or null

--- src/App.js
+++ src/App.js
@@ -1,47 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import AddTodoForm from './AddTodoForm';
-import TodoList from './TodoList';
+import React, { useState, useEffect } from "react";
+import AddTodoForm from "./AddTodoForm";
+import TodoList from "./TodoList";
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+	const [todoList, setTodoList] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-      new Promise((resolve, reject) =>
-        setTimeout(() => {
-          resolve({ data: { todoList: JSON.parse(localStorage.getItem('savedTodoList')) || [] }}
-        )}, 2000))
-      .then((result) => {
-        setTodoList(result.data.todoList)
-        setIsLoading(false)
-      })
-  }, [])
-  
-  const addTodo = (newTodo) => {
-      setTodoList([...todoList, newTodo]);
-  }
+	useEffect(() => {
+		new Promise((resolve, reject) =>
+			setTimeout(() => {
+				resolve({
+					data: {
+						todoList: JSON.parse(localStorage.getItem("savedTodoList")) || [],
+					},
+				});
+			}, 2000)
+		).then((result) => {
+			setTodoList([...result.data.todoList]);
+			setIsLoading(false);
+		});
+	}, []);
 
-  const removeTodo = (id) => {
-    const updatedTodos = todoList.filter(todo => todo.id !== id);
-    setTodoList(updatedTodos);
-  }
+	const addTodo = (newTodo) => {
+		setTodoList([...todoList, newTodo]);
+	};
 
-  useEffect(() => {
-    if(!isLoading) {
-      localStorage.setItem('savedTodoList', JSON.stringify(todoList))
-    }},[todoList])
+	const removeTodo = (id) => {
+		const updatedTodos = todoList.filter((todo) => todo.id !== id);
+		setTodoList(updatedTodos);
+	};
 
-  return (
-   <> 
-     <h1>Todo List</h1>
-     <AddTodoForm onAddTodo={addTodo} />
-     {isLoading ? (
-        <p>Loading...</p> 
-        ):(
-        <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>
-      )}
-   </>
-  );
+	useEffect(() => {
+		if (!isLoading) {
+			localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+		}
+	}, [todoList]);
+
+	return (
+		<>
+			<h1>Todo List</h1>
+			<AddTodoForm onAddTodo={addTodo} />
+			{isLoading ? (
+				<p>Loading...</p>
+			) : (
+				<TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+			)}
+		</>
+	);
 }
 
 export default App;
