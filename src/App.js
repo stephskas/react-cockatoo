1cc787def7c82f71256fd95043a825b3a676fc3d
stephskas <steph.sakai@gmail.com>
2023-01-23 11:35:56 -0800

    setting up Routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddTodoForm from "./AddTodoForm";
 import TodoList from "./TodoList";
+import styles from "./App.module.css";
 
 function App() {
 	const [todoList, setTodoList] = useState([]);
@@ -39,15 +40,26 @@ function App() {
 	}, [todoList, isLoading]);
 
 	return (
-		<>
-			<h1>Todo List</h1>
-			<AddTodoForm onAddTodo={addTodo} />
-			{isLoading ? (
-				<p>Loading...</p>
-			) : (
-				<TodoList todoList={todoList} onRemoveTodo={removeTodo} />
-			)}
-		</>
+		<BrowserRouter>
+			<Routes>
+				<Route
+					exact
+					path="/"
+					element={
+						<>
+							<h1>Todo List</h1>
+							<AddTodoForm onAddTodo={addTodo} />
+							{isLoading ? (
+								<p>Loading...</p>
+							) : (
+								<TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+							)}
+						</>
+					}
+				/>
+				<Route path="/new" element={<h1>New Todo List</h1>} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 
 function TodoListItem({ todo, onRemoveTodo }) {
 	return (
